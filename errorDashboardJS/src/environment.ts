ec9e7cafdff1a8fbaa54200a2c61e9dc4f7b69f9
Daywick <derekli11204@gmail.com>
2024-08-16 16:56:28 -0400

    submodule push

--- errorDashboardJS/src/environment.ts
+++
@@ -1,1 +0,0 @@
-export const baseUrl = "http://higuard-error-dashboard.shuttleapp.rs/api/error";
--- errorDashboardJS/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { ErrorDashboardClient } from "./apiClient";
-
-export { ErrorDashboardClient, ErrorDashboardClient as default };
-export const sendError = ErrorDashboardClient.sendError;
-export const overrideConfigs = ErrorDashboardClient.overrideConfigs;
